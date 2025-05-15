f4eda63df8a747aaf1495216d6cd6b3225f59c5e
Leonardo Maximiliano <leomaxomio@gmail.com>
2025-05-15 13:25:57 -0600

    El login ya jala blyattttt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
     alias(libs.plugins.android.application) apply false
-
-    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version ("2.0.1")
-
-
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1"
+    id("com.google.gms.google-services") version "4.4.2" apply false // ✅ esto es lo que faltaba
 }
